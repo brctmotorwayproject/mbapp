6de33c5842407d602d1f99aff255b2a8ffa44404
hornoo <rchrdhorne1@gmail.com>
2015-10-27 09:54:41 +1300

    REST API
    Began creating input for data base, working on sanitization of data

--- locations.php
+++ locations.php
@@ -4,6 +4,34 @@ include '/connection/connect.inc.php';
 
 
 
+
+if(isset($_POST['upDate'])){
+
+	//echo ($_POST['upDate']);
+	if(is_int($_POST['messageOrUpdate'])){
+        $messageOrUpdate = $_POST['messageOrUpdate'];
+    }
+
+    if(is_int($_POST['latT'])){
+        $latT = $_POST['latT'];
+    }
+
+    if(is_int($_POST['longT'])){
+        $messageOrUpdate = $_POST['LongT'];
+    }
+
+
+
+	$message =  strip_tags(trim($_POST['message']) );
+	echo "$message";
+
+}else{
+	echo json_encode(getData($connection));
+}
+
+
+
+
 function getData($connection){
 
     //Get data from last 6 hours of votes
@@ -26,7 +54,7 @@ function getData($connection){
 	return $data;
 }
 
-echo json_encode(getData($connection));
+//echo json_encode(getData($connection));
 
 ?>
 

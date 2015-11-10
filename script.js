648e528c084f03289e67fe1daf1f109629f03af5
hornoo <rchrdhorne1@gmail.com>
2015-11-10 23:21:47 +1300

    Highlight blueskin bay, changed initial map zoom level

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ $( document ).on( "pageinit", "#map-page", function() {
 	// eg var LatLng = new google.maps.LatLng(123456 , 123456); 
     function drawMap(latlng) {
         var myOptions = {
-            zoom: 10,
+            zoom: 12,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
@@ -56,7 +56,17 @@ $( document ).on( "pageinit", "#map-page", function() {
 		//drawing map on webpage @ id map-canvas
         map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 		
-
+		var blueskinArea = new google.maps.Circle({
+		    strokeColor: '#FF0000',
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: '#FF0000',
+			fillOpacity: 0.35,
+			map: map,
+			center:{ lat:-45.7307748, lng: 170.5857255},
+			radius: 6000
+		
+		});
 		
 		//This waits for the map to be at idle after loading the web page. then Runs what ever is inside first set of brackets.
 		google.maps.event.addListenerOnce( map, 'idle', function() {
